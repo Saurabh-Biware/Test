4433f5bec0e523d5887857abe317b53fa9880233
Biware <saurabh.biware@oerlikon.com>
2025-08-11 14:26:34 +0200

    Add coverage and linting to CI pipeline3

--- test_main.py
+++ test_main.py
@@ -2,11 +2,10 @@ import unittest
 
 from main import hello_world
 
-
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, World Saurabh!")
 
-
 if __name__ == "__main__":
     unittest.main()
+# Add a newline after this line
