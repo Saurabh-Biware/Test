4615e9bf6ac6c9904cae213c817d16d70afd6ea7
Biware <saurabh.biware@oerlikon.com>
2025-08-11 16:52:05 +0200

    changes

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ def hello_world():
 if __name__ == "__main__":
     print(hello_world())
 # Add a newline after this line
+
+# <-- Add a blank line here (just press Enter at the end)
--- test_main.py
+++ test_main.py
@@ -2,10 +2,15 @@ import unittest
 
 from main import hello_world
 
+
 class TestHelloWorld(unittest.TestCase):
+
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, World Saurabh!")
+
 
 if __name__ == "__main__":
     unittest.main()
 # Add a newline after this line
+
+# <-- Add a blank line here (just press Enter at the end)
